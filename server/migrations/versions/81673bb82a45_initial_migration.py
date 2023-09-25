f6238acaef720c3d19f0ccabf38dfcb0c61de0c1
Brian Ashman <brian.ashman@comcast.net>
2023-09-25 11:30:28 -0700

    Creates initial empty DB migration

---
+++ server/migrations/versions/81673bb82a45_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial Migration
+
+Revision ID: 81673bb82a45
+Revises: 
+Create Date: 2023-09-25 11:29:42.171159
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '81673bb82a45'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
