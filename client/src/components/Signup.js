bf3abf95ff67adf21ef7bdd74d162fbb3c3f1bb7
Brian Ashman <brian.ashman@comcast.net>
2023-10-01 21:54:50 -0700

    Implements SignUp.js CSS styling and form updates

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import '../stylesheets/SignUpForm.css'
 
 function Signup({ games, user, setUser }) {
 
   return (
     <>
       <NavBar games={games} user={user} setUser={setUser} />
-      <div className='login-form-page'>
-        <form className='login-form'>
-          <h1 className='form-title'>Re:Flex Games</h1>
-          <label className='form-label' htmlFor='username'>Username: </label>
+      <div className='signup-form-page'>
+        <form className='signup-form'>
+          <h1 className='form-title'>Create Your Account</h1>
+          <p>Power Up Your Re:Flex</p>
+          <label className='form-label' htmlFor='username'>Username </label>
           <input
             type='text'
             id='username'
-            placeholder='Enter username'
+            placeholder="Username"
             autoComplete='off'
             >
           </input>
-          <label className='form-label' htmlFor='password'>Password: </label>
+          <label className='form-label' htmlFor='email'>Email </label>
+          <input 
+            type='text'
+            id='email'
+            placeholder='name@company.com'
+            autoComplete='off'
+            >
+          </input>
+          <label className='form-label' htmlFor='password'>Password </label>
           <input 
             type='text'
             id='password'
-            placeholder='Enter password'
-            autoComplete='current-password'
+            placeholder='Password'
+            autoComplete='off'
             >
           </input>
-          <button id='login-button' type='submit' value='Login'>Login</button>
-          <span className="signup-link">Don't have an account? <a href="/account_signup">Sign up!</a></span>
+          <label className='form-label' htmlFor='password_confirm'>Confirm Password </label>
+          <input 
+            type='text'
+            id='password_confirm'
+            placeholder='Confirm Password'
+            autoComplete='off'
+            >
+          </input>
+          <button id='signup-button' type='submit' value='Login'>Sign Up!</button>
+          <span className="login-link">Already have an account? <a href="/login">Login</a></span>
         </form>
       </div>
     </>
